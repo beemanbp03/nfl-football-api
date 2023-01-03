43358bdc8bd88e39f0be1e12f9cafb028bf36bf2
beemanbp03 <boulder.payne.beeman@gmail.com>
2023-01-03 08:26:35 -0600

    set up two test arrays and started work on test cases

--- test/init.test.js
+++ test/init.test.js
@@ -2,6 +2,40 @@ const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
 
+apiFirstRequest = [
+    {
+        title: "Dolphins Lose Again!",
+        url: "www.url.com/dolphins-lose-again",
+        pubDate: "12-12-12",
+        thumbnail: "efgh.png",
+        author: ""
+    },
+    {
+        title: "The Bears Still Suck",
+        url: "www.url.com/the-bears-still-suck",
+        pubDate: "12-12-12",
+        thumbnail: "abcd.png",
+        author: ""
+    }
+]
+
+apiSecondRequest = [
+    {
+        title: "The Bears Still Suck",
+        url: "www.url.com",
+        pubDate: "12-12-12",
+        thumbnail: "abcd.png",
+        author: ""
+    },
+    {
+        title: "Tom Brady Wants To Play Another Year",
+        url: "www.url.com/tom-brady-wants-to-play-another-year",
+        pubDate: "12-12-12",
+        thumbnail: "kdks.png",
+        author: ""
+    }
+]
+
 describe('Sum of two numbers', () => {
     it ('Should add two numbers then return the sum"', () => {
         x = 0;
@@ -14,3 +48,15 @@ describe('Sum of two numbers', () => {
         assert.equal(2, sumOfNumbers(1,1), `${x} + ${y} = ${sumOfNumbers(x,y)}`);
     })
 })
+
+
+//DATABASE tests
+describe("")
+
+describe("Insert article object successfully", async () => {
+
+})
+
+describe('Check Database For Identical Article Titles', async () => {
+
+});
